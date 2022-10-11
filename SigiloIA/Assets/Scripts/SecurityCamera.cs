089af4aa7bc4d13461c24fb3e9190c72a87cb0a0
GoragarXGameDev <goragarxgamedev@gmail.com>
2022-10-11 18:24:46 +0200

    camera adjusted to function at all scenarios

--- SigiloIA/Assets/Scripts/SecurityCamera.cs
+++ SigiloIA/Assets/Scripts/SecurityCamera.cs
@@ -4,9 +4,22 @@ using UnityEngine;
 
 public class SecurityCamera : MonoBehaviour
 {
-    [SerializeField] float rotationAngle;           //Angulo de giro de la cámara
-    public float rotationSpeed;                     //Velocidad de giro de la cámara
-    public int communicationRange;                  //Alcance para enviar una señal a otros enemigos
+    [TextArea] public string description;
+
+    [SerializeField] float rotationAngle;                   //Angulo de giro de la cámara
+    public float rotationSpeed;                             //Velocidad de giro de la cámara
+    public int communicationRange;                          //Alcance para enviar una señal a otros enemigos
+
+    private float from;
+    private float to;
+
+    private void Start()
+    {
+        from =  transform.rotation.eulerAngles.y - rotationAngle / 2;
+        to = transform.rotation.eulerAngles.y + rotationAngle / 2;
+
+        Debug.Log(from + "," + to);
+    }
 
     private void Update()
     {
@@ -18,6 +31,45 @@ public class SecurityCamera : MonoBehaviour
     // -----------------------------
     void RotateCamera()
     {
+        //Obtener angulo acutal
+        float currentAngle = transform.rotation.eulerAngles.y;
+
+        //Cambiar dirección si el angulo actual sobrepasa
+        //el angulo máximo (to) o el mínimo (from)
+
+        ///tiene que existir una formula general para sacar esto, pero
+        ///soy un inutil incapaz de deducir cómo sacarlo.
+        ///He escrito manualmente los 3 casos posibles:
+        ///from y to estan entre 0 y 360
+        ///from "se pasa" negativamente
+        ///to "se pasa" positivamente
+
+        if (from >= 0 && to >= 0) //caso estandar
+        {
+            if (currentAngle < from || currentAngle > to)
+            {
+                rotationSpeed *= -1;
+            }
+        }
+        
+        if (from < 0) //caso si from "se pasa"
+        {
+
+            if (currentAngle < 360 + from && currentAngle > to)
+            {
+                rotationSpeed *= -1;
+            }
+        }
+
+        if (to > 360) //caso si to "se pasa"
+        {
+            if (currentAngle < from && currentAngle < to - 360)
+            {
+                rotationSpeed *= -1;
+            }
+        }
+
+        //Código para rotar
         transform.Rotate(0, rotationSpeed * Time.deltaTime, 0, Space.Self);
     }
 
@@ -30,6 +82,7 @@ public class SecurityCamera : MonoBehaviour
 
         //Se comunica con todos los enemigos (layer 9 = enemies) en un rango determinado
         Collider[] enemiesNearby = Physics.OverlapSphere(transform.position, communicationRange, 9);
+
         foreach (Collider enemy in enemiesNearby)
         {
             Debug.Log("Enemy nearby found");
@@ -41,7 +94,6 @@ public class SecurityCamera : MonoBehaviour
     // -----------------------------
     void OnDrawGizmosSelected()
     {
-        Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, communicationRange);
     }
 }
